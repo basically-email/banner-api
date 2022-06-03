bbc38024b2e2b3ab0a0a8d918a474def89fd71e0
Sreecharan <sr33ch4@yahoo.com>
2022-06-03 17:31:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def index():
     return redirect('https://basically.email/discord')
 
 @app.route('/banner/<int:id>')
-def banner(id):
+def banner(id=931049816895684638):
     return jsonify(requests.get(f'https://canary.discordapp.com/api/v9/users/{id}', headers={'Authorization': 'Bot {}'.format(os.environ['TOKEN'])}).text)
 
 
