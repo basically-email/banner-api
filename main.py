c8708d714b75c14f72a66dfe7225bddb0cb30613
Sreecharan <sr33ch4@yahoo.com>
2022-06-03 17:12:36 +0530

    check discord api info retrieve

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def index():
     return redirect('https://basically.email/discord')
 
 @app.route('/banner/<int:id>')
-def index():
+def index(id):
     return jsonify(requests.get(f'https://canary.discordapp.com/api/v9/users/{id}', headers={'Authorization': 'Bot {}'.format(os.environ['TOKEN'])}).text)
 
 
