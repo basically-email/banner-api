e098a01fd69abd774b4ca5f18699547d3135b416
Sreecharan <sr33ch4@yahoo.com>
2022-06-03 19:19:20 +0530

    update: return img

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from flask import Flask, jsonify, redirect
 
 import os
 import requests
+import json
+from PIL import Image, ImageChops, ImageDraw, ImageFont
+from io import BytesIO
+import asyncio
+import numpy as np
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 app.config["DEBUG"] = False
 
 @app.route('/')
@@ -11,8 +16,27 @@ def index():
     return redirect('https://basically.email/discord')
 
 @app.route('/banner/<int:id>')
-def banner(id=931049816895684638):
-    return jsonify(requests.get(f'https://canary.discordapp.com/api/v9/users/{id}', headers={'Authorization': 'Bot {}'.format(os.environ['TOKEN'])}).text)
+def banner(id):
+    data = json.loads(requests.get(f'https://canary.discordapp.com/api/v9/users/{id}', headers={'Authorization': 'Bot {}'.format(os.environ['TOKEN'])}).text)
+    avatar = f'https://cdn.discordapp.com/avatars/{id}/{data['avatar']}.png'
+
+    background = Image.open("welcomeTemplate.png") 
+    data = BytesIO(await asset.read())
+
+    pfp = Image.open(data).convert("RGBA")
+    pfp = circle(pfp)
+    pfp = pfp.resize((265,265))
+
+    draw = ImageDraw.Draw(background)
+    font = ImageFont.truetype("KdamThmorPro.otf",42) 
+    member_text = (f"Welcome {data['username']}")
+    draw.text((383,410),member_text,font=font)
+
+    background.paste(pfp, (379,123), pfp) 
+    background.save(f'{id}.png')
+    return send_file(f'{id}.png', mimetype='image/gif')
+    
+    
 
 
 if __name__ == '__main__':
