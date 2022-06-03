a6a039b11963510ed815fbe3277cdbe88f12fe2e
Sreecharan <sr33ch4@yahoo.com>
2022-06-03 17:11:57 +0530

    check info retrieve from discord api

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect
+
 import os
+import requests
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return redirect('https://basically.email/discord')
+
+@app.route('/banner/<int:id>')
+def index():
+    return jsonify(requests.get(f'https://canary.discordapp.com/api/v9/users/{id}', headers={'Authorization': 'Bot {}'.format(os.environ['TOKEN'])}).text)
 
 
 if __name__ == '__main__':
